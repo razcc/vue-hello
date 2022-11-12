711d17bc906cf544ad115aaac89e015ce1017b44
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-11-12 19:17:44 +0100

    Titolo e immagine da java

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-var app = new Vue(
-    {
-        el: "#root",
-        data: {
-            
-        }
-    }
+var app = new Vue({
+    el: "#root",
+    data: {
+        titolo: "Vue Title",
+        classeDinamica: "text-primary",
+        imgDinamica: "https://i0.wp.com/www.estate-romana.it/wp-content/uploads/2019/03/foro-romano.jpg?resize=640%2C426&ssl=1"
+    },
+    methods: {
 
-)
-    
+    }
+})
+
